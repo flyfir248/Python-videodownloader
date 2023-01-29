9a972c78f9b520e3c2d1c030e20ecf3ed69d8ef0
@johnya0 <anoopjohny2000@gmail.com>
2023-01-29 10:02:29 +0100

    commit subbranch

--- main.py
+++ main.py
@@ -1,13 +1,44 @@
+import sys
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QLabel, QLineEdit, QPushButton, QVBoxLayout, QWidget
 from pytube import YouTube
+from PyQt5.QtWidgets import QFileDialog
 
-# Get the link of the video you want to download
-link = "https://www.youtube.com/watch?v=6HRON_6yPII&ab_channel=NationalGeographicIndia"
+class YouTubeDownloader(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.init_ui()
 
-# Create a YouTube object
-yt = YouTube(link)
+    def init_ui(self):
+        self.setWindowTitle("YouTube Downloader")
 
-# Get the video with the highest resolution
-video = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+        # Create a label for the URL textbox
+        url_label = QLabel("Enter video URL:")
 
-# Download the video
-video.download()
+        # Create a textbox for the user to enter the video URL
+        self.url_textbox = QLineEdit()
+
+        # Create a button to initiate the download
+        download_button = QPushButton("Download")
+        download_button.clicked.connect(self.download_video)
+
+        # Create a layout to hold the label, textbox, and button
+        layout = QVBoxLayout()
+        layout.addWidget(url_label)
+        layout.addWidget(self.url_textbox)
+        layout.addWidget(download_button)
+
+        # Set the layout for the main window
+        self.setLayout(layout)
+
+    def download_video(self):
+        url = self.url_textbox.text()
+        yt = YouTube(url)
+        stream = yt.streams.first()
+        save_path = QFileDialog.getSaveFileName(self, "Save Video", "", "All Files (*);;MP4 Files (*.mp4)")
+        stream.download(save_path[0])
+
+app = QApplication(sys.argv)
+youtube_downloader = YouTubeDownloader()
+youtube_downloader.show()
+sys.exit(app.exec_())
